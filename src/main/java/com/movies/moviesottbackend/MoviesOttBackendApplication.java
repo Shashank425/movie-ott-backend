9657d39053be5e6680a92b76bda0f9ce6305ea05
Shashank425 <shashank.kathavate@gmail.com>
2024-08-21 13:15:32 -0500

    first push

---
+++ src/main/java/com/movies/moviesottbackend/MoviesOttBackendApplication.java
@@ -0,0 +1,13 @@
+package com.movies.moviesottbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesOttBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviesOttBackendApplication.class, args);
+    }
+
+}
